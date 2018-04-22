70b8a08f18bde1cbc1ef44f94f59cb3735e94dfe
Mr. Sponti <38351453+MrSponti@users.noreply.github.com>
2018-04-22 18:02:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
  * Config file for speech recognition modul 's2cmd.js'
  *
- * (c) Mr. Sponti           latest update: 13.04.2018
+ * (c) Mr. Sponti           latest update: 22.04.2018
  *
  * This file contains the configuration parameter for the module as well as the word defintions
  * to build a formated and unique instruction from a spoken text.
@@ -10,15 +10,21 @@
  * ==> 4 elements:      zone:object:attribute:command                
  *        example:      livingroom:light:mainlight:on
  *
- *  The combination of zone and object is seen as a service point and has a assigned device address and communication parameter
- *  to send the final command to that device.
+ *  The combination of zone and object is seen as a service point and has a assigned target device address and 
+ *  communication protocol parameter used to send the final command to that device. 
  *  Example:
- *                         spoken text:    "pimatic, schalte im Esszimmer das Licht ein"       
+ *                         spoken text:    "pimatic, schalte im Esszimmer das Licht ein"
+ * 
  *       will generate the instruction:     livingroom:light:mainlight:on
  *     and mapped to the short command:     'press buttonFLon'
- *     the service point 'livingroom:light' from the config.devices table is than used to send the short command to the device
+ *     the service point 'livingroom:light' from the config.devices table is than used to get the communication protocol 
+ *     which is used to send the short command to the device
  *
  * ==>  'livingroom:light': {'host': 'pimatic',   'port': 80,   'service': 'pimatic', 'capability': 0},
+ *
+ *      for a better understanding of the used approach you can start the s2cmd.js in debug mode:
+ *
+ * ==>  s2cmd.js debug noaction
  *
  */
 var config ={}
@@ -32,20 +38,20 @@ config.numbers = {}
 
 // basic config parameter 
 config = {
-    'sonusLedGPIO':       0,                                            // recording indication LED connected to GPIO17
-    'sonusHotword':       'pimatic',                                    // hotwords to start recording
-    'sonusHotwordFile':  '/resources/pimatic.pmdl',                     // snowboy hotword file
-    'sonusSensitivity':   '0.6',                                        // hotword recording sensitivity
+    'sonusLedGPIO':       21,                                           // recording indication LED connected to GPIO17
+    'sonusHotword':       'pimatic',                                    // hotword to start recording
+    'sonusHotwordFile':   '/resources/pimatic.pmdl',
+    'sonusSensitivity':   '0.5',                                        // hotword recording sensitivity
     'sonusAudioGain':     2.0,                                          // hotword recording audio gain
     'logPrompt':          'Ja, bitte?',                                 // prompt to display on MagicMirror
     'googleLanguage':     'de_DE',
-    'googleProjectId':    'yourGoogleProjecId',                         // google authorization credentials
-    'googleKeyFilename':  'yourGoogleKeyFilename',
+    'googleProjectId':    'yourGoogleProjectID',                        // google authorization credentials
+    'googleKeyFilename':  'resources/yourGoogleKeyfileName.json',       
     'pimaticUser':        'yourPimaticUser',                            // pimatic user credentials
     'pimaticPasswd':      'yourPimaticUserPassword'                 
 }        
 
-/**
+/**********************************************************************************************************************
  *  German dictionary of words used to define final instruction by filtering spoken text
  **/
 //
@@ -101,7 +107,6 @@ config.cmd = {
 //     spoken word : [ translation to attribute, default object, (optional - default zone)]
 //
 config.attribute =   {
-    'stehlampe':    ['floorlamp', 'licht', 'livingroom'],
     'mitteilungen': ['message', 'postit','diningroom'],
     'kanal':        ['channel','fernseher'],
     'sender':       ['channel','-'],
@@ -124,9 +129,11 @@ config.attribute =   {
 //
 //     spoken word : [ translation to object, default zone, default attribute]     
 config.object = {
+    'stehlampe':   ['lightPlus','livingroom','floorlamp'],
     'licht':       ['light', 'diningroom', 'mainlight'],
-    'fernseher':   ['tv','livingroom','power'],
+    'fernseher':   ['tv','livingroom','channel'],
     'radio':       ['radio','diningroom', '-'],
+    'amadeus':     ['radio','office', '-'],
     'monitor':     ['monitor', 'diningroom', '-'],
     'rolladen':    ['shutter','bedroom', 'direction'],
     'photos':      ['picture','diningroom', 'vacation'],
@@ -158,7 +165,7 @@ config.numbers = {
     'neun':   ['nine','9'],
     'zehn':   ['ten','10']
 }
-         
+//      
 // Instruction to short command mapping table: ==> map full instruction to a short command
 //
 config.cmdMap = {  
@@ -169,10 +176,6 @@ config.cmdMap = {
 //   
     'livingroom:light:mainlight:on':            'press buttonWZon',
     'livingroom:light:mainlight:off':           'press buttonWZon',
-    'livingroom:light:floorlamp:on':            'press onoff',
-    'livingroom:light:floorlamp:off':           'press onoff',
-    'livingroom:light:floorlamp:up':            'press dim-plus',
-    'livingroom:light:floorlamp:down':          'press dim-min',
 //
     'garage:garage:door:open':                  'press open-garage',
     'garage:garage:door:close':                 'press close-garage',
@@ -180,25 +183,51 @@ config.cmdMap = {
 
 //  Service Points (target network devices and related protocols)
 //  -------------------------------------------------------------
-//  capability = 0 --> a short instruction will not send to the last used target device (must be greater 0)
-//              >0 --> a short instruction will be send to the last used target device
-//               2 --> device accepts optional words for detailing the attribute information
+//  capability = 0 --> a short instruction will not send to the last used service point (must be greater 0)
+//              >0 --> a short instruction can be send to the last used service point
+//               2 --> service point (target device) accepts optional words for detailing the attribute information
 //
-//   service point         | hostname             | port       | service name         | device capability |
+//   service point          | hostname           | port        | service name            device capability |
 //
 config.devices =   {
     'diningroom:speechlog': {'host': 'Infoboard', 'port': 5588, 'service': 'SPEECHLOG', 'capability': 0},
-    'diningroom:postit':    {'host': 'Infoboard', 'port': 5588, 'service': 'POSTIT', 'capability': 0},
+    'diningroom:postit':    {'host': 'Infoboard', 'port': 5588, 'service': 'POSTIT',    'capability': 0},
     'diningroom:picture':   {'host': 'Infoboard', 'port': 5588, 'service': 'PHOTOSHOW', 'capability': 2},
     'diningroom:monitor':   {'host': 'Infoboard', 'port': 5577, 'service': 'mm motion', 'capability': 0},
-    'diningroom:radio':     {'host': 'fidelio',   'port': 5577, 'service': 'audio', 'capability': 1},
-    'diningroom:light':     {'host': 'pimatic',   'port': 80,   'service': 'pimatic', 'capability': 0},    
-    'livingroom:radio':     {'host': 'pimatic',   'port': 5577, 'service': 'audio', 'capability': 1},
-    'livingroom:tv':        {'host': 'pimatic',   'port': 5577, 'service': 'tv', 'capability': 1},
-    'livingroom:light':     {'host': 'pimatic',   'port': 80,   'service': 'pimatic', 'capability': 0},
-    'office:radio':         {'host': 'cms',       'port': 5577, 'service': 'audio', 'capability': 1},
-    'bedroom:shutter':      {'host': 'pimatic',   'port': 5577, 'service': 'jarolift', 'capability': 0},  
-    'garage:garage':        {'host': 'pimatic',   'port': 80,   'service': 'pimatic', 'capability': 0},
-    'outdoor:camera':       {'host': 'Infoboard', 'port': 5588, 'service': 'RTSP', 'capability': 1}
-}           
+    'diningroom:radio':     {'host': 'fidelio',   'port': 5577, 'service': 'audio',     'capability': 1},
+    'diningroom:light':     {'host': 'pimatic',   'port': 80,   'service': 'pimatic',   'capability': 0},    
+    'livingroom:radio':     {'host': 'pimatic',   'port': 5577, 'service': 'audio',     'capability': 1},
+    'livingroom:tv':        {'host': 'pimatic',   'port': 5577, 'service': 'tv',        'capability': 1},
+    'livingroom:light':     {'host': 'pimatic',   'port': 80,   'service': 'pimatic',   'capability': 1},
+    'livingroom:lightPlus': {'host': 'pimatic',   'port': 5577, 'service': 'tradfri',   'capability': 1},
+    'office:radio':         {'host': 'amadeus',   'port': 5577, 'service': 'audio',     'capability': 1},
+    'bedroom:shutter':      {'host': 'pimatic',   'port': 5577, 'service': 'jarolift',  'capability': 0},  
+    'garage:garage':        {'host': 'pimatic',   'port': 80,   'service': 'pimatic',   'capability': 0},
+    'outdoor:camera':       {'host': 'Infoboard', 'port': 5588, 'service': 'RTSP',      'capability': 1}
+}
+/**********************************************************************************************************************
+ *  Wireless remote device: Zeepin TZ MX6 2,4 GHz Wireless Remote
+ **/
+//
+// keycodes: 
+//
+config.KeyCode = {
+   28: 'ok',
+  103: 'up',
+  105: 'previous',
+  106: 'next',
+  108: 'down',
+  114: 'minus',
+  115: 'plus',
+  116: 'power',
+  158: 'return',
+  194: 'micro',
+}
+// input device files:  use 'ls -al /dev/input/by-path' to identify device files of your wireless remote
+//
+config.eventFiles = {
+  'kbd':   'event0',
+  'mouse': 'event1',
+}
+//*********************************************************************************************************************     
 module.exports = config
